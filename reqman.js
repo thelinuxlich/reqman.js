cd730d28e71169d388713d9bc779a36b2208820b
Alisson Cavalcante Agiani <thelinuxlich@gmail.com>
2011-03-04 10:33:12 -0300

    Adding methods to find and kill requests by regex

--- reqman.js
+++ reqman.js
@@ -1,117 +1,104 @@
-/**
-* Container de requests AJAX
-* @name RQ
-* @class
-*/
-var RQ = {
-    /**
-    * Função para executar um callback antes de adicionar uma request
-    * @function
-    */
+(function() {
+  this.RQ = {
     beforeAdd: null,
-    /**
-    * Função para executar um callback após adicionar uma request
-    * @function
-    */
     afterAdd: null,
-    /**
-    * Função para executar um callback antes de remover uma request
-    * @function
-    */
     beforeRemove: null,
-    /**
-    * Função para executar um callback depois de remover uma request
-    * @function
-    */
     afterRemove: null,
     container: [],
-    /**
-    * Função para excluir todas as requests do container
-    * @function
-    */
     killAll: function() {
-       for(var i in RQ.container) {
-           if(RQ.container[i]["request"] !== false)
-               RQ.container[i]["request"].abort();
-       }
-       RQ.container = [];
+      var i;
+      for (i in RQ.container) {
+        if (RQ.container[i]["request"] !== false) {
+          RQ.container[i]["request"].abort();
+        }
+      }
+      return RQ.container = [];
     },
-    /**
-    * Função para adicionar uma request ao container
-    * @param req  Request a ser adicionada
-    * @param req_id  Um id único para a request
-    * @function
-    */
-    add: function(req,req_id) {
-        if(typeof RQ.beforeAdd === "function")
-            RQ.beforeAdd();
-        req_id = req_id || "rq_"+RQ.container.length + 1;
-        if(req !== false)
-            RQ.container.push({id: req_id,request: req});
-        if(typeof RQ.afterAdd === "function")
-            RQ.afterAdd();
+    add: function(req, req_id) {
+      if (typeof RQ.beforeAdd === "function") {
+        RQ.beforeAdd();
+      }
+      req_id = req_id || ("rq_" + (RQ.container.length + 1));
+      if (req !== false) {
+        RQ.container.push({
+          id: req_id,
+          request: req
+        });
+      }
+      if (typeof RQ.afterAdd === "function") {
+        return RQ.afterAdd();
+      }
     },
-    /**
-    * Função para buscar uma request no container
-    * @param condition  Recebe a request ou o id da request como parâmetro de busca
-    * @function
-    */
     find: function(condition) {
-        var index = -1;
-        var field = "";
-        field = (condition["request"] !== undefined ? "request" : "id");
-        for(var i in RQ.container) {
-           if(RQ.container[i][field] === condition[field]) {
-                index = i;
-                break;
-           }
+      var field, i, index;
+      index = -1;
+      field = "";
+      field = condition["request"] !== void 0 ? "request" : "id";
+      for (i in RQ.container) {
+        if (RQ.container[i][field] === condition[field]) {
+          index = i;
+          break;
         }
-        return index;
+      }
+      return index;
     },
-    /**
-    * Função para remover uma request do container
-    * @param condition  Recebe a request ou o id da request como parâmetro de busca
-    * @function
-    */
+    findByRegex: function(condition) {
+      var i, indexes;
+      indexes = [];
+      for (i in RQ.container) {
+        if (i["id"].match(condition)) {
+          indexes.push(i);
+        }
+      }
+      return indexes;
+    },
     remove: function(condition) {
-        if(typeof RQ.beforeRemove === "function")
-            RQ.beforeRemove();
-        var index = RQ.find(condition);
-        if(index > -1)
-            RQ.container.splice(index,1);
-        if(typeof RQ.afterRemove === "function")
-            RQ.afterRemove();
+      var index;
+      if (typeof RQ.beforeRemove === "function") {
+        RQ.beforeRemove();
+      }
+      index = typeof condition === "object" ? RQ.find(condition) : condition;
+      if (index > -1) {
+        RQ.container.splice(index, 1);
+      }
+      if (typeof RQ.afterRemove === "function") {
+        return RQ.afterRemove();
+      }
     },
-    /**
-    * Função para excluir uma request do container(ela será automaticamente abortada)
-    * @param condition  Recebe a request ou o id da request como parâmetro de busca
-    * @function
-    */
     kill: function(condition) {
-        var index = RQ.find(condition);
-        if(index > -1) {
-            if(RQ.container[index]["request"] !== false)
-                RQ.container[index]["request"].abort();
-            RQ.remove(condition);
+      var index;
+      index = RQ.find(condition);
+      if (index > -1) {
+        if (RQ.container[index]["request"] !== false) {
+          RQ.container[index]["request"].abort();
         }
+        return RQ.remove(condition);
+      }
     },
-    /**
-    * Função para exibir todas as requests no container
-    * @function
-    */
-    showAll: function() {
-        var requests = "";
-        for(var i in RQ.container) {
-            requests += "id: "+RQ.container[i]["id"]+",request:"+
-            RQ.container[i]["request"]+"\n";
+    killByRegex: function(condition) {
+      var i, indexes, _ref, _results;
+      indexes = RQ.findByRegex(condition);
+      _results = [];
+      for (i = 0, _ref = indexes.length; (0 <= _ref ? i <= _ref : i >= _ref); (0 <= _ref ? i += 1 : i -= 1)) {
+        if (RQ.container[indexes[i]]["request"] !== false) {
+          RQ.container[indexes[i]]["request"].abort();
         }
-        return requests;
+        _results.push(RQ.remove(RQ.container[indexes[i]]["id"]));
+      }
+      return _results;
+    },
+    showAll: function() {
+      var i, requests;
+      requests = "";
+      for (i in RQ.container) {
+        requests += "id: " + RQ.container[i]['id'] + ",request: " + RQ.container[i]['request'] + "\n";
+      }
+      return requests;
     }
-};
-
-
-/** Remover a request do container após sua resposta */
-$(document).ajaxComplete(function(e,xhr,settings) {
-    RQ.remove({request:xhr});
-});
-
+  };
+  $(document).ajaxComplete(function(e, xhr, settings) {
+    return RQ.remove({
+      request: xhr
+    });
+  });
+}).call(this);
